e0cbb9e37905b981bf349bfdd0b9b891dcdb96e3
jaquel <jaquelrey@gmail.com>
2022-10-07 17:03:06 -0400

    Logic not working, saving in case I break it more

--- script.js
+++ script.js
@@ -2,11 +2,9 @@
 // node modules 
 const fs = require('fs');
 const inquirer = require('inquirer');
-const createTeam = require('./src/create-team.js');
 
 //func to generate html after data collection is finished
-const createHTML = require('./src/create-team.js')
-
+const createTeam = require('./src/create-team.js');
 
 //class constructors
 const Manager = require('./lib/Manager');
@@ -20,17 +18,17 @@ const teamArray = [];
 async function createEmployees(role) {
 
     //changing the name message on a prompt conditionally 
-    //("when:" would make 3 prompts where I just need one)
+    //('when:' would make 3 prompts where I just need one)
 
     let nameMsgs = 'Who\'s the project manager?'
-    let pNutGal = "You can't manage yourself.... Try again!"
+    let pNutGal = 'You can\'t manage yourself.... Try again!'
 
-    if (role === "Intern") {
+    if (role === 'Intern') {
         nameMsgs = 'Who\'s the new intern?'
-        pNutGal = "I know they\'re an intern, but you still need to give them a name! Try again."
-    } else if (role === "Engineer") {
+        pNutGal = 'I know they\'re an intern, but you still need to give them a name! Try again.'
+    } else if (role === 'Engineer') {
         nameMsgs = 'Who\'s the new engineer?'
-        pNutGal = "Code isn't going to write itself, name your engineer! Try again."
+        pNutGal = 'Code isn\'t going to write itself, name your engineer! Try again.'
     }
 
 
@@ -43,16 +41,18 @@ async function createEmployees(role) {
             //and can include non consecutive hypens and spaces
             validate: function (answer) {
                 let valid = /^\p{L}+(?:[- ]\p{L}+)*$/.test(answer)
-                if (answer.length < 1 || answer === " " || answer === "-") {
+                if (answer.length < 1 || answer === ' ' || answer === '-') {
 
-                    return console.log(pNutGal);
+                    console.log(pNutGal);
+                    return false
 
                 } else if (valid != true) {
 
-                    return console.log("A name can only include letters, non-consecutive hyphens, and single spaces.")
-                
+                    console.log('A name can only include letters, non-consecutive hyphens, and single spaces.')
+                    return false
+                } else {
+                return true
                 }
-                return true;
             }
         },
         {
@@ -62,10 +62,12 @@ async function createEmployees(role) {
             //answer must be number
             validate: function (answer) {
                 if (isNaN(answer) === true) {
-
-                    return console.log("Employee ID has to be a number.");
+                    
+                    console.log('Employee ID has to be a number.');
+                    return false
+                } else {
+                    return true
                 }
-                return true;
             }
         },
         {
@@ -77,9 +79,12 @@ async function createEmployees(role) {
                 let valid = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)
                 if (valid != true) {
 
-                    return console.log("Not a valid email address format, please re-enter employee\'s email.");
+                    console.log('Not a valid email address format, please re-enter employee\'s email.');
+                    return false
+
+                } else {
+                return true
                 }
-                return true;
             }
         },
         {
@@ -91,9 +96,11 @@ async function createEmployees(role) {
             validate: function (answer) {
                 if (Number.isInteger(answer) === true) {
                     
-                    return console.log("Office number must be a number with no decimal values.");
+                    console.log('Office number must be a number with no decimal values.');
+                    return false
+                } else {
+                return true
                 }
-                return true;
             }
         },
         {
@@ -104,15 +111,19 @@ async function createEmployees(role) {
             //same validation as name
             validate: function (answer) {
                 let valid = /^\p{L}+(?:[- ]\p{L}+)*$/.test(answer)
-                if (answer.length < 1 || answer === " " || answer === "-") {
+                if (answer.length < 1 || answer === ' ' || answer === '-') {
 
-                    return console.log("Intern needs to have a school! Try again.");
+                    console.log('Intern needs to have a school! Try again.');
+                    return false
 
                 } else if (valid != true) {
 
-                    return console.log("School name must only include letters, non-consecutive hyphens, and single spaces.");
+                    console.log('School name must only include letters, non-consecutive hyphens, and single spaces.');
+                    return false
+
+                } else {
+                return true
                 }
-                return true;
             }
         },
         {
@@ -131,38 +142,46 @@ async function createEmployees(role) {
                 let valid = /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/.test(github)
                 if (valid != true) {
 
-                    return console.log("Not a valid email address format, please re-enter employee\'s Github.");
-                
-                }
+                    console.log('Not a valid email address format, please re-enter employee\'s Github.');
+                    return false
+                } else {
                 return true
+                }
             }
         },
     ])
 
-
-    let { name, id, email, office, school, github } = answers
+    //detructure answers object into seperate vars
+    //employee to be created from answers, conditional to assign
 
     let emp
 
-    if (role === "Intern") {
+    if (role === 'Intern') {
+        let { name, id, email, school } = answers
         emp = new Intern(name, id, email, school)
-    } else if (role === "Engineer") {
+    } else if (role === 'Engineer') {
+        let { name, id, email, github } = answers
         emp = new Engineer(name, id, email, github)
     } else {
+        let { name, id, email, office } = answers
         emp = new Manager(name, id, email, office)
     }
+
+    //push created employee object to array
+    console.log('==========================')
+    console.log('Adding new employee to team....')
+    console.log('==========================')
 
     teamArray.push(emp)
 };
 
 
-
 //checks if user wants to exit prompting and generate team with current array
 async function confirmTeam() {
 
-    console.log("Here's your current team:")
+    console.log('Here\'s your current team:')
     console.table(teamArray)
-    console.log("==========================")
+    console.log('==========================')
 
     //query user
 
@@ -186,57 +205,67 @@ async function confirmTeam() {
 
 }
 
+//choose next employee role
+async function chooseRole() {
+    
+    let choose = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'emprole',
+            message: 'What\'s the role of your next team member?',
+            choices: ['Intern', 'Engineer'],
+        }
+    ])
+
+    //generate next employee, then check for next action
+
+    if (choose.emprole === 'Intern') {
+        console.log('Creating a new Intern...')
+        console.log('==========================')
+        await createEmployees('Intern')
+        confirmTeam()
+    } else {
+        console.log('Creating a new Engineer...')
+        console.log('==========================')
+        await createEmployees('Engineer')
+        confirmTeam()
+    }
+
+
+}
+
 //calls html generation, and returns it to fs
 async function creatingPage() {
     let teamed = await createTeam.createHTML(teamArray);
-    //making the manager's name part of the file name, removing spaces in the case of "Firstname Lastname"
+    //making the manager's name part of the file name, removing spaces in the case of 'Firstname Lastname'
     //I should also check for characters that aren't allowed as filenames...
     let checkedName = (teamArray[0].name).replaceAll(' ', '')
     //path / filename to use in fs call
     let pathAndName = (`./created/${checkedName}-team.html`)
-    console.log("Creating your team's page....");
-    console.log("==========================")
+    console.log('Creating your team\'s page....');
+    console.log('==========================')
 
     return fs.writeFile(pathAndName, teamed, (error) => {
         if (error) throw error;
-        console.log("Success! Team page created and stored in the \"created\" folder!")
+        console.log('Success! Team page created and stored in the \"created\" folder!')
     });
 }
 
 
 
 async function generateTeam() {
-    // let { name, id, email, office, school, github } = await createEmployees();
-
-    const manager = await createEmployees("Manager")
-    const intern = await createEmployees("Intern");
-    const engineer = await createEmployees("Engineer");
-    //checks role of second employee
-    let comf = await confirmTeam("Manager")
-
-
-
-    //make manager, push to array
-    //make 2nd employee (intern or manager)
-    //confirm to add members or generate html
-    //else recurse
-
-
-
-    // console.table(teamArray)
-    do {
-        if (comf[0] === "Intern") {
-
-            await confirmTeam()
-        }
-        if (comf[0] === "Engineer") {
-
-            await confirmTeam()
-        }
-    } while (comf[1] != "Finished Team")
-
+    //generate manager once- calling chooseRole starts the rest of the stack
+    console.log('Creating a Manager...')
+    console.log('==========================')
+    await createEmployees('Manager');
+    await chooseRole()
 }
 
 // begin stack
-
 generateTeam()
+// stack order:
+// 1. generateTeam creates the manager, calls chooseRole
+// 2. select employee type and await creation, call confirmTeam
+// 3. if select add employee, repeat step 2... else call creatingPage
+// 4. call createTeam and pass in employee array to generate html
+// 5. on return, use fs and write new file
--- src/create-team.js
+++ src/create-team.js
@@ -1,133 +1,147 @@
-//using a dispatch table to generate employee cards
-//first time using these so I've just built the functions inside the table
-//if all goes well I could seperate them out 
-let dispatchTable = {
-    Engineer: function (engineer) {
-        return `
-    <div class="columns m-2">
-        <div class="column">
-            <div class="card">
-                <header class="card-header has-background-info">
-                  <p class="card-header-title has-text-white">
-                    Engineer
-                  </p>
-                </header>
-                <div class="card-content is-flex is-justify-content-center">
-                    <table class="table">
-                        <tbody>
-                            <tr>
-                                <th>Name</th>
-                                <td>${engineer.name}</td>
-                            </tr>
-                            <tr>
-                                <th>ID</th>
-                                <td>${engineer.id}</td>
-                            </tr>
-                            <tr>
-                                <th>Email</th>
-                                <td> <a href="mailto:${engineer.email}">${engineer.email}</a></td>
-                            </tr>
-                            <tr>
-                                <th>Github</th>
-                                <td><a href="https://github.com/${engineer.github}">${engineer.github}</a></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-              </div>
-        </div>`
-    },
-
-    Intern: function (intern) {
-        return `
-    <div class="columns m-2">
-        <div class="column">
-            <div class="card">
-                <header class="card-header has-background-info">
-                  <p class="card-header-title has-text-white">
-                    Intern
-                  </p>
-                </header>
-                <div class="card-content is-flex is-justify-content-center">
-                    <table class="table">
-                        <tbody>
-                            <tr>
-                                <th>Name</th>
-                                <td>${intern.name}</td>
-                            </tr>
-                            <tr>
-                                <th>ID</th>
-                                <td>${intern.id}</td>
-                            </tr>
-                            <tr>
-                                <th>Email</th>
-                                <td> <a href="mailto:${intern.email}">${intern.email}</a></td>
-                            </tr>
-                            <tr>
-                                <th>School</th>
-                                <td>${intern.school}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-              </div>
-        </div>`
-    },
-
-    Manager: function (manager) {
-        return `
-    <div class="columns m-2">
-        <div class="column">
-            <div class="card">
-                <header class="card-header has-background-info">
-                  <p class="card-header-title has-text-white">
-                    Manager
-                  </p>
-                </header>
-                <div class="card-content is-flex is-justify-content-center">
-                    <table class="table">
-                        <tbody>
-                            <tr>
-                                <th>Name</th>
-                                <td>${manager.name}</td>
-                            </tr>
-                            <tr>
-                                <th>ID</th>
-                                <td>${manager.id}</td>
-                            </tr>
-                            <tr>
-                                <th>Email</th>
-                                <td> <a href="mailto:${manager.email}">${manager.email}</a></td>
-                            </tr>
-                            <tr>
-                                <th>Office #</th>
-                                <td>${manager.office}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-              </div>
-        </div>`
- }
+function createEngineer(member) {
+    return `
+<div class="columns m-2">
+    <div class="column">
+        <div class="card">
+            <header class="card-header has-background-info">
+              <p class="card-header-title has-text-white">
+                Engineer
+              </p>
+            </header>
+            <div class="card-content is-flex is-justify-content-center">
+                <table class="table">
+                    <tbody>
+                        <tr>
+                            <th>Name</th>
+                            <td>${member.name}</td>
+                        </tr>
+                        <tr>
+                            <th>ID</th>
+                            <td>${member.id}</td>
+                        </tr>
+                        <tr>
+                            <th>Email</th>
+                            <td> <a href="mailto:${member.email}">${member.email}</a></td>
+                        </tr>
+                        <tr>
+                            <th>Github</th>
+                            <td><a href="https://github.com/${member.github}">${member.github}</a></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+          </div>
+    </div>`
 }
-//the meat of this whole page is this function
+
+function createIntern(member) {
+    return `
+<div class="columns m-2">
+    <div class="column">
+        <div class="card">
+            <header class="card-header has-background-info">
+              <p class="card-header-title has-text-white">
+                Intern
+              </p>
+            </header>
+            <div class="card-content is-flex is-justify-content-center">
+                <table class="table">
+                    <tbody>
+                        <tr>
+                            <th>Name</th>
+                            <td>${member.name}</td>
+                        </tr>
+                        <tr>
+                            <th>ID</th>
+                            <td>${member.id}</td>
+                        </tr>
+                        <tr>
+                            <th>Email</th>
+                            <td> <a href="mailto:${member.email}">${member.email}</a></td>
+                        </tr>
+                        <tr>
+                            <th>School</th>
+                            <td>${member.school}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+          </div>
+    </div>`
+}
+
+function createManager(member) {
+    return `
+<div class="columns m-2">
+    <div class="column">
+        <div class="card">
+            <header class="card-header has-background-info">
+              <p class="card-header-title has-text-white">
+                Manager
+              </p>
+            </header>
+            <div class="card-content is-flex is-justify-content-center">
+                <table class="table">
+                    <tbody>
+                        <tr>
+                            <th>Name</th>
+                            <td>${member.name}</td>
+                        </tr>
+                        <tr>
+                            <th>ID</th>
+                            <td>${member.id}</td>
+                        </tr>
+                        <tr>
+                            <th>Email</th>
+                            <td> <a href="mailto:${member.email}">${member.email}</a></td>
+                        </tr>
+                        <tr>
+                            <th>Office #</th>
+                            <td>${member.office}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+          </div>
+    </div>`
+}
+
+
+
+//empty array for the returned card html
+let completedTeam = []
+
+function genCard(member) {
+
+    //attempt to use a dispatch table to generate employee cards
+    let dispatchTable = {
+        Engineer: createEngineer(member),
+
+        Intern: createIntern(member),
+
+        Manager: createManager(member)
+    }
+
+
+    //get the employee role (will serve as dispatch table key)
+    let type = member.getRole();
+    //generate card
+    let card = dispatchTable[type](member)
+    //push the card returned to the array created earlier
+    completedTeam.push(card)
+}
+
 //array of team member objects passed in from script.js
-function createHTML(team) {
-    //empty array to populate with the returned employee cards (html strings)
-    let completedTeam = []
+async function createHTML(team) {
 
     //for each employee object in the team
-    team.forEach(genCard);
+    //generate a card
+    await team.forEach((member) => {
+        genCard(member)
+    })
 
-    function genCard(member){
-        //get the employee role (which will serve as dispatch table key)
-        let type = member.getRole();
-        //call the command to gen the correct card
-        //(employee obj passed in to access values)
-        let card = dispatchTable[type](member)
-        //push the card returned to the array created earlier
-        completedTeam.push(card)
-    }
+    //then return the finished html
+
     return fullPage(completedTeam.join(" "));
 }
 
